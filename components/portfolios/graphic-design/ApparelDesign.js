143926a62bf706bc88389ed4910465d04ec15bce
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-03-28 12:36:51 +0200

    adding apparel design

--- components/portfolios/graphic-design/ApparelDesign.js
+++ components/portfolios/graphic-design/ApparelDesign.js
@@ -1,5 +1,53 @@
 import React from "react";
+import styles from "../../../styles/ApparelDesign.module.scss";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Masonry from "@mui/lab/Masonry";
+import { styled } from "@mui/material/styles";
+
+const Label = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(0.5),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  borderBottomLeftRadius: 0,
+  borderBottomRightRadius: 0,
+}));
 
 export default function ApparelDesign() {
-  return <div>ApparelDesign</div>;
+  const IMAGES = [
+    { img: "../images/apparel-design/1.png" },
+    { img: "../images/apparel-design/2.png" },
+    { img: "../images/apparel-design/3.png" },
+    { img: "../images/apparel-design/7.webp" },
+    { img: "../images/apparel-design/5.png" },
+    { img: "../images/apparel-design/6.png" },
+    { img: "../images/apparel-design/4.png" },
+  ];
+  return (
+    <>
+      <h1>APPAREL DESIGN</h1>
+
+      <Box sx={{ width: "100%", minHeight: 829 }}>
+        <Masonry columns={3} spacing={2}>
+          {IMAGES.map((item, index) => (
+            <div key={index} className={styles.imageContainer}>
+              <img
+                src={`${item.img}?w=162&auto=format`}
+                srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
+                loading="lazy"
+                style={{
+                  borderBottomLeftRadius: 4,
+                  borderBottomRightRadius: 4,
+                  display: "block",
+                  width: "100%",
+                }}
+              />
+            </div>
+          ))}
+        </Masonry>
+      </Box>
+    </>
+  );
 }
