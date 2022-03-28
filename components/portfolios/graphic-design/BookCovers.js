12d75e59a45d26ef3c23b0e30587ceb8bc6aaee3
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-03-28 11:59:27 +0200

    adding book covers

--- components/portfolios/graphic-design/BookCovers.js
+++ components/portfolios/graphic-design/BookCovers.js
@@ -1,5 +1,21 @@
 import React from "react";
+import styles from "../../../styles/BookCovers.module.scss";
 
 export default function BookCovers() {
-  return <div>BookCovers</div>;
+  return (
+    <>
+      <h1>BOOK COVERS</h1>
+      <div className={styles.imageContainer}>
+        <img src="../images/book-covers/1.png" />
+        <img src="../images/book-covers/2.png" />
+        <img src="../images/book-covers/3.png" />
+        <img src="../images/book-covers/4.png" />
+        <img src="../images/book-covers/5.png" />
+        <img src="../images/book-covers/6.png" />
+        <img src="../images/book-covers/7.png" />
+        <img src="../images/book-covers/9.jpg" />
+        <img src="../images/book-covers/10.png" />
+      </div>
+    </>
+  );
 }
