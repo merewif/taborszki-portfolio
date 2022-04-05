b630acaa13e4de77848353b164ceeba23a5de826
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-04-05 16:04:16 +0200

    dinamically fetching images from specified folder

---
+++ components/portfolios/graphic-design/Gallery.js
@@ -0,0 +1,19 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import useSWR from "swr";
+import styles from "../../../styles/Gallery.module.scss";
+
+export default function Gallery({ files, galleryStyle }) {
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const { data } = useSWR("/api/images", fetcher);
+
+  return (
+    <>
+      <div className={galleryStyle}>
+        {!data && "Loading..."}
+        {data &&
+          data[files].map((imgPath, i) => <img src={imgPath} key={i} alt="" />)}
+      </div>
+    </>
+  );
+}
