d4cf87a71e016ea235d1bbe06c5489c59825f41e
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-04-05 16:02:54 +0200

    filling landing page

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -1,5 +1,11 @@
 import React from "react";
+import styles from "../styles/About.module.scss";
 
 export default function AboutMe() {
-  return <div>AboutMe</div>;
+  return (
+    <div className={styles.aboutContainer}>
+      <div>Text</div>
+      <div>Contact</div>
+    </div>
+  );
 }
