bb1622e6bfe597044468056dde4f17038990e0ad
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-04-07 05:11:06 +0200

    Changing the order of portfolio elements.

--- components/ModuleFactory.tsx
+++ components/ModuleFactory.tsx
@@ -34,9 +34,9 @@ export default function ModuleFactory({ currentModule }: ModuleFactoryProps) {
   if (currentModule === 'frontend-portfolio') {
     return (
       <>
-        <Plotter />
-        <VisualNovel />
         <Loretracker />
+        <VisualNovel />
+        <Plotter />
       </>
     );
   }
@@ -198,6 +198,28 @@ export default function ModuleFactory({ currentModule }: ModuleFactoryProps) {
   if (currentModule === 'translation-portfolio') {
     return (
       <>
+        <div
+          style={{
+            marginInline: '20vw',
+            textAlign: 'justify',
+            textAlignLast: 'center',
+            marginBottom: '20px',
+          }}
+        >
+          <p>
+            Over a 3 year period I taught myself the basics of graphic design,
+            with a heavy emphasis on typography, using primarily Adobe Photoshop
+            and Adobe Illustrator.
+          </p>
+          <p>
+            Originally it started out from the need to create visually
+            captivating thumbnails for blogposts. I then went on to create
+            designs for various items of clothing that enjoyed a modicum of
+            success - customers bought them all over the world from the United
+            States to India. Lastly I went on to design book covers for dozens
+            of books.
+          </p>
+        </div>
         <Publishing />
         <Excerpts />
         <ListOfTranslatedBooks />
